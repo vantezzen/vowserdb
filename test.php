75c51ec1fd4665931339402b6a243e303ddcf588
Bennett Hollstein <bennett@aternos.org>
2017-03-22 20:19:41 +0100

    Added test.php

---
+++ test.php
@@ -0,0 +1,42 @@
+<?php
+// This file will check all important functions of vowserDB to see if everything still works
+error_reporting(E_ALL);
+echo "testing vowserdb...";
+include("vowserdb.php");
+echo "<br />Encryption is ";
+echo vowserdb::$encrypt ? 'activated' : 'deactivated';
+echo "<br />Backups are ";
+echo vowserdb::$dobackup ? 'activated' : 'deactivated';
+echo "<br />The lock mechanism is ";
+echo vowserdb::$disablelock ? 'deactivated' : 'activated';
+echo "<br />vowserdb check: ";
+print_r(vowserdb::check());
+vowserdb::CREATE("testing", array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"));
+vowserdb::INSERT(array("1" => "select me pls", "3" => "Heyy", "4" => "ahsdkahsdgasdagsjdgajsd", "8" => "a", "9" => "xxxx"), "testing");
+echo "<br />SELECT test: ";
+if (json_encode(vowserdb::SELECT("testing", array("1" => "select me pls"))) == '[["","select me pls","","Heyy","ahsdkahsdgasdagsjdgajsd","","","","a","xxxx"]]') {
+    echo "PASSED";
+} else {
+    echo "Not as expected: ";
+    print_r(vowserdb::SELECT("testing", array("1" => "select me pls")));
+}
+vowserdb::UPDATE("testing", array("1" => "select me pls pls"), array("1" => "select me pls"));
+vowserdb::DELETE("testing", array("1" => "select me pls pls"));
+vowserdb::RENAME("testing", "1", "NEW");
+vowserdb::DROP("testing");
+vowserdb::CREATE("table1", array("user", "text", "addedOn"));
+vowserdb::CREATE("table2", array("username", "password", "mail"));
+vowserdb::relationship("table1", "user", "table2", "username");
+vowserdb::INSERT(array("text" => "This is a test message", "addedOn" => "Today", "user" => "vantezzen"), "table1");
+vowserdb::INSERT(array("username" => "vantezzen", "password" => "1234", "mail" => "mail@example.com"), "table2");
+echo "<br />Relationship test: ";
+if (json_encode(vowserdb::SELECT("table1")) == '[{"user":[{"username":"vantezzen","password":"1234","mail":"mail@example.com"}],"text":"This is a test message","addedOn":"Today"}]') {
+    echo "PASSED";
+} else {
+    echo "Not as expected: ";
+    print_r(vowserdb::SELECT("table1"));
+}
+vowserdb::TABLES();
+vowserdb::DROP("table1");
+vowserdb::DROP("table2");
+echo "<br />testing done";
